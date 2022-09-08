5e29f777d5bebbf63d957732365c45f6f2655437
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-09-09 00:04:00 +0700

    add widget destination_card.dart

---
+++ lib/ui/widgets/destination_card.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:airplane/shared/theme.dart';
+
+class DestinationCard extends StatelessWidget {
+  final String name;
+  final String city;
+  final String imgUrl;
+  final double rating;
+
+  const DestinationCard({
+    Key? key,
+    required this.name,
+    required this.imgUrl,
+    required this.city,
+    this.rating = 0.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 323,
+      width: 200,
+      decoration: BoxDecoration(
+        color: kWhiteColor,
+        borderRadius: BorderRadius.circular(defaultRadius),
+      ),
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+      margin: EdgeInsets.only(
+        left: defaultMargin,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 220,
+            width: 180,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(defaultRadius),
+              image: DecorationImage(
+                image: AssetImage(imgUrl),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                height: 30,
+                width: 55,
+                decoration: BoxDecoration(
+                  color: kWhiteColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(defaultRadius),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      margin: const EdgeInsets.only(right: 4),
+                      child: Image.asset(
+                        'assets/icon_star.png',
+                      ),
+                    ),
+                    Text(
+                      rating.toString(),
+                      style: blackTextStyle.copyWith(
+                        fontWeight: semibold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                city,
+                style: greyTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: light,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
