aee02a282cf787f79a9e6d9c8efb8d92305ced4a
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-09-15 13:17:28 +0700

    add success page

---
+++ lib/ui/pages/success_page.dart
@@ -0,0 +1,53 @@
+import 'package:airplane/ui/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:airplane/shared/theme.dart';
+
+class SuccessPage extends StatelessWidget {
+  const SuccessPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 300,
+              margin: const EdgeInsets.only(bottom: 80),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image_success.png'),
+                ),
+              ),
+            ),
+            Text(
+              'Well Booked',
+              style: blackTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Are you ready to explore the new\nworld of experiences?',
+              textAlign: TextAlign.center,
+              style: greyTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: light,
+              ),
+            ),
+            const SizedBox(height: 50),
+            CustomButton(
+              width: 220,
+              title: 'My Bookings',
+              onpressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
