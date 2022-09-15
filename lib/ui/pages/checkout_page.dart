742c18e73446fa6da36878e2e638e6f0918dbd16
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-09-15 18:37:27 +0700

    create new widgets internal and external

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -1,3 +1,6 @@
+import 'package:airplane/ui/widgets/booking_detail_item.dart';
+import 'package:airplane/ui/widgets/custom_button.dart';
+import 'package:airplane/ui/widgets/interest_item.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane/shared/theme.dart';
 
@@ -84,6 +87,7 @@ class CheckoutPage extends StatelessWidget {
           borderRadius: BorderRadius.circular(defaultRadius),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // NOTE: DESTINATION TILE
             Row(
@@ -143,7 +147,181 @@ class CheckoutPage extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.only(
+                bottom: 16,
+              ),
+              child: Text(
+                'Booking Details',
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semibold,
+                ),
+              ),
+            ),
+            const BookingDetailItem(
+              title: 'Traveler',
+              valueText: '2 Person',
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'Seat',
+              valueText: 'A3, B3',
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'Insurance',
+              valueText: 'YES',
+              valueColor: kGreenColor,
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'Refundable',
+              valueText: 'NO',
+              valueColor: kRedColor,
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'VAT',
+              valueText: '45%',
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'Price',
+              valueText: 'IDR 8.500.690',
+              mBot: 16,
+            ),
+            const BookingDetailItem(
+              title: 'Grand Total',
+              valueText: 'IDR 12.000.000',
+              valueColor: kPrimaryColor,
+            ),
           ],
+        ),
+      );
+    }
+
+    Widget paymentDetails() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Payment Details',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semibold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Container(
+                  height: 70,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    color: kPrimaryColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: kPrimaryColor.withOpacity(0.5),
+                        blurRadius: 50,
+                        offset: const Offset(0, 10),
+                      ),
+                    ],
+                    gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xff5445E5),
+                        Color(0xff998EFD),
+                      ],
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 24,
+                        width: 24,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icon_plane.png'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 6),
+                      Text(
+                        'Pay',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'IDR 80.400.000',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'Current Balance',
+                      style: greyTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget btnPay() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: CustomButton(
+          title: 'Pay Now',
+          onpressed: () {
+            Navigator.pushNamed(context, '/success');
+          },
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(
+          top: 30,
+          bottom: 30,
+        ),
+        child: Text(
+          'Terms and Conditions',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
         ),
       );
     }
@@ -158,6 +336,9 @@ class CheckoutPage extends StatelessWidget {
           children: [
             route(),
             bookingDetail(),
+            paymentDetails(),
+            btnPay(),
+            tacButton(),
           ],
         ),
       ),
