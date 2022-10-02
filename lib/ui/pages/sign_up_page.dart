85849bc10c50bd791cbef613ab7094c4196a9110
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-10-02 15:41:10 +0700

    add textEditingController, blocConsumer and state

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,10 +1,18 @@
+import 'package:airplane/cubit/auth_cubit.dart';
 import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:airplane/ui/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane/shared/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpPage extends StatelessWidget {
-  const SignUpPage({Key? key}) : super(key: key);
+  SignUpPage({Key? key}) : super(key: key);
+
+  final TextEditingController nameController = TextEditingController(text: '');
+  final TextEditingController emailController = TextEditingController(text: '');
+  final TextEditingController passwordController =
+      TextEditingController(text: '');
+  final TextEditingController hobbyController = TextEditingController(text: '');
 
   @override
   Widget build(BuildContext context) {
@@ -23,30 +31,73 @@ class SignUpPage extends StatelessWidget {
 
     Widget inputSection() {
       Widget fullNameInput() {
-        return const CustomTextFormField(
+        return CustomTextFormField(
           title: 'Full Name',
           hintText: 'Input your full name',
+          controller: nameController,
         );
       }
 
       Widget emailInput() {
-        return const CustomTextFormField(
+        return CustomTextFormField(
           title: 'Email Address',
           hintText: 'Input your email address',
+          controller: emailController,
         );
       }
 
       Widget passwordInput() {
-        return const CustomTextFormField(
+        return CustomTextFormField(
           title: 'Password',
           hintText: 'Input your password',
           obsecureText: true,
+          controller: passwordController,
         );
       }
 
       Widget hobbyInput() {
-        return const CustomTextFormField(
-            title: 'Hobby', hintText: 'Input your hobby');
+        return CustomTextFormField(
+          title: 'Hobby',
+          hintText: 'Input your hobby',
+          controller: hobbyController,
+        );
+      }
+
+      Widget registerButton() {
+        return BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthSuccess) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/bonus', (route) => false);
+            } else if (state is AuthFailed) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: kRedColor,
+                  content: Text(state.error),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return CustomButton(
+              marginTop: 30,
+              title: 'Register',
+              onpressed: () {
+                context.read<AuthCubit>().signUp(
+                      email: emailController.text,
+                      password: passwordController.text,
+                      name: nameController.text,
+                      hobby: hobbyController.text,
+                    );
+              },
+            );
+          },
+        );
       }
 
       Widget tacButton() {
@@ -87,13 +138,7 @@ class SignUpPage extends StatelessWidget {
             emailInput(),
             passwordInput(),
             hobbyInput(),
-            CustomButton(
-              marginTop: 30,
-              title: 'Register',
-              onpressed: () {
-                Navigator.pushNamed(context, '/bonus');
-              },
-            ),
+            registerButton(),
             tacButton(),
           ],
         ),
